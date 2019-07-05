b7df39bbe3d8a908849c93fc68ad1efd927e59cb
xahiru <xahiru@gmail.com>
2019-07-05 16:51:53 +0800

    before changing the loop iterators

--- impt.py
+++ impt.py
@@ -8,11 +8,12 @@ from surprise.model_selection import train_test_split
 from collections import defaultdict
 import copy as cp
 from surprise.dump import dump
+import pandas as pd
 
 
 class ImpliciTrust(AlgoBase):
 
-    def __init__(self, trainset, testset, algo, final_algo, compare=None, binary=False, parellel=False, random_state=100):
+    def __init__(self, trainset, testset, algo, final_algo, compare=None, binary=True, parellel=False, random_state=100):
 
         # Always call base method before doing anything.
         AlgoBase.__init__(self)
@@ -32,43 +33,79 @@ class ImpliciTrust(AlgoBase):
         self.compare = compare if compare is not None else 0
  
     def fit(self, trainset):
-    	print('fitting')
-    	self.final_algo.fit(self.final_algo_trainset)
-    	self.orginal_pu = cp.deepcopy(self.final_algo.pu)
-    	self.orginal_qi = cp.deepcopy(self.final_algo.qi)
-    	self.set_ur(trainset)
-    	self.set_ir(trainset)
-    	self.algo.fit(self.trainset)
-    	self.pu = self.algo.pu
-    	self.qi = self.algo.qi
-    	print('done fitting with modified trainset')
-    	
-    def set_ur(self, trainset):
-    	ir = defaultdict(list)
-    	for item_x in range(trainset.n_items):
-    		item_list = []
-    		item_pop = len(trainset.ir[item_x])/trainset.n_items
-    		for user, rating in trainset.ir[item_x]:
-    			u_pop = 1/len(trainset.ur[user])
-    			item_list.append((user,u_pop * item_pop))
-    		ir[item_x] = item_list
-    	self.trainset.ir = ir
-    	print('done set_ur')
+        print('fitting')
+        self.final_algo.fit(self.final_algo_trainset)
+        print('copying')
+        self.orginal_pu = cp.deepcopy(self.final_algo.pu)
+        self.orginal_qi = cp.deepcopy(self.final_algo.qi)
+        print('setting ur n ir')
+        self.set_ur(trainset, self.binary)
+        self.set_ir(trainset, self.binary)
+        print('final fit')
+        self.algo.fit(self.trainset)
+        self.pu = self.algo.pu
+        self.qi = self.algo.qi
+        print('done fitting with modified trainset')
 
-    def set_ir(self, trainset):
-    	ur = defaultdict(list)
-    	for user_x in range(trainset.n_users):
-    		user_list = []
-    		user_pop = 1/len(trainset.ur[user_x])
-    		for item, rating in trainset.ur[user_x]:
-    			item_pop = len(trainset.ir[item])/trainset.n_items
-    			user_list.append((item, item_pop * user_pop))
-    		ur[user_x] = user_list
-    	self.trainset.ur = ur
-    	print('done set_ir')
+    def set_ur(self, trainset, binary=True):
+        ir = defaultdict(list)
+        if binary:
+            for item_x in range(trainset.n_items):
+                item_list = []
+                item_pop = len(trainset.ir[item_x])/trainset.n_items
+                for user, rating in trainset.ir[item_x]:
+                    u_pop = 1/len(trainset.ur[user])
+                    item_list.append((user,u_pop * item_pop))
+                    # print((user,u_pop * item_pop))
+                ir[item_x] = item_list
+            self.trainset.ir = ir
+        else:
+            for item_x in range(trainset.n_items):
+                item_list = []
+                item_pop = len(trainset.ir[item_x])/trainset.n_items
+                for user, rating in trainset.ir[item_x]:
+                    u_pop = rating/len(trainset.ur[user])
+                    item_list.append((user,u_pop * item_pop))
+                    # print((user,u_pop * item_pop))
+                ir[item_x] = item_list
+            self.trainset.ir = ir
+    def set_ir(self, trainset, binary=True):
+        ur = defaultdict(list)
+        if binary:
+            for user_x in range(trainset.n_users):
+                user_list = []
+                user_pop = 1/len(trainset.ur[user_x])
+                for item, rating in trainset.ur[user_x]:
+                    item_pop = len(trainset.ir[item])/trainset.n_items
+                    user_list.append((item, item_pop * user_pop))
+                    # print((item, item_pop * user_pop))
+                ur[user_x] = user_list
+            self.trainset.ur = ur
+            print('done set_ir')
+        else:
+            for user_x in range(trainset.n_users):
+                user_list = []
+                user_pop_r_len = len(trainset.ur[user_x])
+                for item, rating in trainset.ur[user_x]:
+                    user_pop = rating/user_pop_r_len
+                    item_pop = len(trainset.ir[item])/trainset.n_items
+                    user_list.append((item, item_pop * user_pop))
+                    # print((item, item_pop * user_pop))
+                ur[user_x] = user_list
+            self.trainset.ur = ur
+            print('done set_ir')
 
-    def single_user_trustlist(self, u):
-    	return self.trainset.ur[u]
+    def single_user_trustlist(self, u, i):
+        print('retreving u trust')
+        print(str(u))
+        print(str(i))
+        for it,t in self.trainset.ur[u]:
+            print('it')
+            print(str(it))
+            print(str(t))
+            if it==i:
+                return t
+        return 0
 
     def single_item_trustlist(self, i):
     	return self.trainset.ir[i]
@@ -88,28 +125,79 @@ class ImpliciTrust(AlgoBase):
     		self.final_algo.qi = self.orginal_qi - self.qi
     		return self.final_algo.estimate(u,i)
 
+
+
 dataset_name ='ml-100k'
 # dataset_name ='jester'
+# dataset_name ='ml-1m'
+# dataset_name ='ml-latest-small'
 compare = 3
 n_factors = 20
 n_epochs = 20
-save = True
+save = False
+binary=True
 data = Dataset.load_builtin(dataset_name)
 trainset, testset = train_test_split(data,random_state=100, test_size=.20)
 algo_name = 'SVDpp_'
 algo = SVDpp(random_state=100)
 final_algo = SVDpp(random_state=100)
-myalgo = ImpliciTrust(trainset, testset, algo, final_algo, compare=compare)
+print('start')
+myalgo = ImpliciTrust(trainset, testset, algo, final_algo, compare=compare, binary=binary)
 myalgo.fit(trainset)
 print(dataset_name)
-# myalgo.single_user_trustlist(543)
+print('compare ='+str(compare))
+myalgo.single_user_trustlist(543, 249)
 # myalgo.single_item_trustlist(249)
+print('myalgo.pu')
+print(myalgo.pu)
+print('myalgo.qi')
+print(myalgo.qi)
+print('binary')
+print(str(binary))
+
 
 predictions = myalgo.test(testset)
-for p in predictions[:100]:
-		# print(t.ur[t.to_inner_uid(p.uid)])
-		print(p)
+for p in predictions[:10]:
+	# print(p)
+    print(p)
+    if int(p[0])== 543:
+        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>543 found')
+        print('543 found')
+    print(myalgo.single_user_trustlist(int(p[0]), int(p[1])))
+    # print(myalgo.single_item_trustlist( int(p[1])))
 accuracy.rmse(predictions)
+accuracy.mae(predictions)
+
+df = pd.DataFrame(predictions, columns=['user_id', 'item_id', 'ratings_ui', 'estimated_Ratings', 'details'])
+def get_Iu(uid):
+    """Return the number of items rated by given user"""
+    try:
+        return len(trainset.ur[trainset.to_inner_uid(uid)])
+    except ValueError:  # user was not part of the trainset
+        return 0
+    
+def get_Ui(iid):
+    """Return the number of users that have rated given item"""
+    try:
+        return len(trainset.ir[trainset.to_inner_iid(iid)])
+    except ValueError:  # item was not part of the trainset
+        return 0 
+df['Iu'] = df.user_id.apply(get_Iu)
+df['Ui'] = df.item_id.apply(get_Ui)
+df['ERROR IN PREDICTION'] = abs(df.estimated_Ratings - df.ratings_ui)
+
+print(df.head())
+best_predictions = df.sort_values(by='ERROR IN PREDICTION')[:10]
+print('best_predictions')
+print(best_predictions)
+worst_predictions = df.sort_values(by='ERROR IN PREDICTION')[-10:]
+print('worst_predictions')
+print(worst_predictions)
+print(dataset_name)
+print('compare ='+str(compare))
+print('binary')
+print(str(binary))
+
 if save:
 	dump('results/'+algo_name+dataset_name+'_n_factors_'+str(n_factors)+'_n_epochs_'+str(n_epochs)+'compare'+str(compare), predictions=predictions, algo=myalgo, verbose=1)
 
---
+++ testtab.py
@@ -0,0 +1,8 @@
+import numpy as np
+print('hello')
+
+def myfunc():
+	print('nonon')
+	print('gain')
+
+myfunc()
